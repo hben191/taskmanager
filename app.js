f6df108021df3ef5b03ce70c1920af976eccea6d
Hamza <benbrahim.hamza@proton.me>
2024-10-15 22:23:35 +0200

    create mongoose connection

--- app.js
+++ app.js
@@ -38,4 +38,14 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+const mongoose = require('mongoose');
+
+// MongoDB connection URI (use your connection string here)
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase';
+
+// Connect to MongoDB
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
 module.exports = app;
