28aaf1779e9096ee10ce1fa0b17d2dc4fd2ebd2a
Hamza <benbrahim.hamza@proton.me>
2024-10-15 22:22:41 +0200

    update docker and bash aliases

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -6,3 +6,6 @@ alias ddown='docker compose down --volumes'
 
 # Execute a shell inside the taskmanager container
 alias dexec='docker exec -it taskmanager-app-1 /bin/bash'
+
+# Run npm start in the taskmanager container
+alias dstart='docker exec -it taskmanager-app-1 npm start'
